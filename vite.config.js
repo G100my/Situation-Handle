3887988adf31eb2b72bc6240807fb05bd1e84a92
G100 <G100my.lan229@gmail.com>
2021-12-11 22:50:45 +0800

    add vite-plugin-pages@beta in vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
+import Pages from 'vite-plugin-pages'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Pages({
+      dirs: 'src/views',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
